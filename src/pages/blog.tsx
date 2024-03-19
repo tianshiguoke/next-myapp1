dc18195b10f577e7c5262c216703541b14280102
Fei860715 <15701200715@163.com>
2024-03-19 15:19:24 +0800

    ~

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -3,39 +3,6 @@ import React from 'react';
 import { InferGetStaticPropsType } from 'next'
 import { promises as fs } from 'fs'
 import path from 'path'
-type Post = {
-    author: string
-    content: string
-    list: []
-}
-export const getStaticProps = async () => {
-    const res = await fetch('http://localhost:3000/api/hello')
-    const posts: any = await res.json()
-
-
-    const postsDirectory = path.join(process.cwd(), 'src/json/')
-    const filenames = await fs.readdir(postsDirectory, 'utf8')
-    const posts1 = filenames.map(async (filename) => {
-        const filePath = path.join(postsDirectory, filename)
-        const fileContents = await fs.readFile(filePath, 'utf8')
-
-        return {
-            id: filename,
-            filename,
-            content: fileContents,
-        }
-    })
-
-
-    return {
-        props: {
-            posts: await Promise.all(posts1),
-            list: posts?.list,
-            a: filenames,
-        },
-        revalidate: 10,
-    }
-}
 
 // export async function getStaticPaths() {
 //     const res = await fetch('http://localhost:3000/api/hello')
@@ -52,11 +19,11 @@ export const getStaticProps = async () => {
 //     return { paths, fallback: 'blocking' }
 // }
 
-interface BlogProps {
-    title?: string;
-    description?: string;
-    posts?: any
-}
+// interface BlogProps {
+//     title?: string;
+//     description?: string;
+//     posts?: any
+// }
 
 // const Blog: React.FC<BlogProps> = ({ posts }) => {
 
