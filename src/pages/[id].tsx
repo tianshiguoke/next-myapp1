8b74ba6bd6685b28b315b5750f14a9dbcab67f81
Fei860715 <15701200715@163.com>
2024-03-19 11:16:46 +0800

    ~

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,37 +1,37 @@
-import React from 'react'
+// import React from 'react'
 
-const User = ({ user }) => {
-    return (
-        <div className="row">
-            <div className="col-md-6 offset-md-3">
-                <div className="card">
-                    <div className="card-body text-center">
-                        <h3>{user.name}</h3>
-                        <p>Email: {user.email} </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+// const User = ({ user }: any) => {
+//     return (
+//         <div className="row">
+//             <div className="col-md-6 offset-md-3">
+//                 <div className="card">
+//                     <div className="card-body text-center">
+//                         <h3>{user.name}</h3>
+//                         <p>Email: {user.email} </p>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
-export async function getStaticPaths() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await res.json()
+// export async function getStaticPaths() {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const users = await res.json()
 
-    const paths = users.map((user) => ({
-        params: { id: user.id.toString() },
-    }))
+//     const paths = users.map((user) => ({
+//         params: { id: user.id.toString() },
+//     }))
 
-    return { paths, fallback: false }
-}
+//     return { paths, fallback: false }
+// }
 
 
-export async function getStaticProps({ params }) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
-    const user = await res.json()
+// export async function getStaticProps({ params }) {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+//     const user = await res.json()
 
-    return { props: { user } }
-}
+//     return { props: { user } }
+// }
 
-export default User
+// export default User
