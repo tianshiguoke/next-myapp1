436fb8cc132c9ba92a3cf4aa6414b1e08b04d702
Fei860715 <15701200715@163.com>
2024-03-19 15:14:00 +0800

    ~

--- src/pages/ExampleDialog.tsx
+++ src/pages/ExampleDialog.tsx
@@ -1,25 +1,19 @@
 // // components/ExampleDialog.js
-// import { useState } from 'react'
+import { useState } from 'react'
 // import { Dialog } from '@reach/dialog'
 // // import VisuallyHidden from '@reach/visually-hidden'
 // // import '@reach/dialog/styles.css'
 
-// function ExampleDialog(props) {
-//     const [showDialog, setShowDialog] = useState(false)
-//     const open = () => setShowDialog(true)
-//     const close = () => setShowDialog(false)
+function ExampleDialog(props: any) {
+    const [showDialog, setShowDialog] = useState(false)
+    const open = () => setShowDialog(true)
+    const close = () => setShowDialog(false)
 
-//     return (
-//         <div>
-//             <button onClick={open}>Open Dialog</button>
-//             <Dialog isOpen={showDialog} onDismiss={close}>
-//                 <button className="close-button" onClick={close}>
-//                     {/* <VisuallyHidden>Close</VisuallyHidden> */}
-//                     <span aria-hidden>×</span>
-//                 </button>
-//                 <p>Hello there. I am a dialog</p>
-//             </Dialog>
-//         </div>
-//     )
-// }
-// export default ExampleDialog
+    return (
+        <div>
+            <button onClick={open}>Open Dialog</button>
+
+        </div>
+    )
+}
+export default ExampleDialog
