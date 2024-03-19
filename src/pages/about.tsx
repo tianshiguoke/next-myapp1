67578e1dd238cf7d5c108147f3c699f64ca78bfe
Fei860715 <15701200715@163.com>
2024-03-19 15:45:33 +0800

    ·

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -15,29 +15,29 @@ function About({ posts }: BlogProps) {
     </div>
 }
 // 此函数在构建时被调用
-export async function getStaticProps(context: any) {
-    console.log(context)
-    // 调用外部 API 获取博文列表
-    const res = await fetch('http://localhost:3000/api/hello')
-    const posts = await res.json()
-    if (!posts) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        }
-        // return {
-        //     notFound: true,
-        // }
-    }
-    // 通过返回 { props: { posts } } 对象，Blog 组件
-    // 在构建时将接收到 `posts` 参数
-    return {
-        props: {
-            posts,
-        },
-    }
-}
+// export async function getStaticProps(context: any) {
+//     console.log(context)
+//     // 调用外部 API 获取博文列表
+//     const res = await fetch('http://localhost:3000/api/hello')
+//     const posts = await res.json()
+//     if (!posts) {
+//         return {
+//             redirect: {
+//                 destination: '/',
+//                 permanent: false,
+//             },
+//         }
+//         // return {
+//         //     notFound: true,
+//         // }
+//     }
+//     // 通过返回 { props: { posts } } 对象，Blog 组件
+//     // 在构建时将接收到 `posts` 参数
+//     return {
+//         props: {
+//             posts,
+//         },
+//     }
+// }
 export default About
 
