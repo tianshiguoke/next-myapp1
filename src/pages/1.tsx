cb91b27a2723335cead9fae08f99112cdaa492e4
Fei860715 <15701200715@163.com>
2024-03-21 10:03:01 +0800

    ~

--- src/pages/1.tsx
+++ src/pages/1.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import Head from 'next/head'
 
-export const config = { amp: true }
+// export const config = { amp: true }
 const names = ['Tim', 'Joe', 'Bel', 'Max', 'Lee']
 const DynamicComponent = dynamic(() => import('./components/Button'))
 export default function Page() {
@@ -11,26 +11,7 @@ export default function Page() {
     return (
         <div>
             <DynamicComponent children='111' />
-            <div>
-                <Head>
-                    <script
-                        async
-                        key="amp-timeago"
-                        custom-element="amp-timeago"
-                        src="https://cdn.ampproject.org/v0/amp-timeago-0.1.js"
-                    />
-                </Head>
 
-                <p>Some time: {date.toJSON()}</p>
-                <amp-timeago
-                    width="0"
-                    height="15"
-                    datetime={date.toJSON()}
-                    layout="responsive"
-                >
-                    .
-                </amp-timeago>
-            </div>
             <input
                 type="text"
                 placeholder="Search"
